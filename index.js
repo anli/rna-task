c47476a629af8feb7dbd2426b48b526357a05ff3
Su Anli <sual0001@gmail.com>
2021-04-25 11:42:46 +0800

    fix(sentry): disable error reporting in dev mode

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ import 'react-native-get-random-values';
 import {name as appName} from './app.json';
 import {App} from './src';
 
-Sentry.init({
-  dsn:
-    'https://1aa238932ec842fa9c746e248eed3fea@o506338.ingest.sentry.io/5734778',
-});
+if (!__DEV__) {
+  Sentry.init({
+    dsn:
+      'https://1aa238932ec842fa9c746e248eed3fea@o506338.ingest.sentry.io/5734778',
+  });
+}
 
 GoogleSignin.configure({
   webClientId: '800010873210-s6t6soj3pk6nbvhd4vdgagdf1jt3ejv1',
